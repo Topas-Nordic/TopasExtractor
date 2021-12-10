320b54227a0ec455b0bb4851ca054378ba36ecd3
Nicolai Haaber Junge <70808555+NicolaiHaaberJunge@users.noreply.github.com>
2021-12-10 02:02:15 +0100

    Update tpextract.py

--- bin/tpextract.py
+++ bin/tpextract.py
@@ -6,24 +6,24 @@ from tpextract import read_topas, extract_big_out, extract_refined, topas_to_csv
 
 def main():
 
-    parser = argparse.ArgumentParser(description="Tool for extracting TP data.")
-    parser.add_argument('topas-out', type=str, help="Path to the topas out file.")
-    parser.add_argument('output', type=str, help="Output filename.")
+    parser = argparse.ArgumentParser(description="Topas Extractor is a tool for extracting refined paramters from Topas .OUT files.")
+    parser.add_argument('topas-out', type=str, help="Path to the Topas .OUT file.")
+    parser.add_argument('output', type=str, help="Output filename. Name of your results file.")
 
     parser.add_argument(
         "-exc",
         type=str, 
-        help="""Parameters to exclude from Topas OUT file. Written as a comma seperated list:
+        help="""Parameters to exclude from Topas OUT file. Written as a comma separated list:
                 E.g: c, a, scale, ...""")
     parser.add_argument(
         "-sct",
         type=str,
-        help="""Parameters to select specifically from Topas OUT file. Written as a commar
+        help="""Parameters to select specifically from Topas OUT file. Written as a comma
                 separated list. 
                 E.g.: c, a, b, ....""")
 
     parser.add_argument('-xdd', action="store_true", help="Include the xdd file name in the extraction.")
-    parser.add_argument('-big', action="store_true", help="For surface refined BIG.INP topas files.")
+    parser.add_argument('-big', action="store_true", help="For surface or parametric refined BIG.OUT Topas files.")
 
 
 
@@ -48,22 +48,9 @@ def main():
     else:
         params = extract_refined(text=tp_text, select=select, exclude=exclude, xdd_include=args_dict['xdd'])
 
-
     topas_to_csv(params=params, output=args_dict['output'])
 
     return
 
-
 if __name__ == "__main__":
-
-
-
-
-    main(
-        tpfile=args_dict['topas-out'],
-        output=args_dict['out'],
-        exclude=exclude,
-        select=select,
-        xdd_include=args_dict['xdd'],
-        big=args_dict['big']
-    )
+    main()
